f80b2e429a1fcb4a952ada9737e5e327529cf32b
prasanna44182 <prasanna44182@gmail.com>
2025-05-26 18:28:50 -0700

    Initial commit

---
+++ NOTES_CICD/notes/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Note
+
+def note_list(request):
+    notes = Note.objects.all()
+    return render(request, 'notes/note_list.html', {'notes': notes})
